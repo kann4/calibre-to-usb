fbc6270e189ad0b1ebe36392e50fae4189db77e2
kann4 <kann4kann4@gmail.com>
2021-01-16 14:24:42 +0800

    allowed run path from cmd (testing version)

--- calibre to usb.py
+++ calibre to usb.py
@@ -1,11 +1,42 @@
+#"calibre_usb" is created on same level as working folder
 from distutils.dir_util import copy_tree
 import os
+import sys
+from send2trash import send2trash
 
-cwd = os.getcwd()
+to_be_del = []
+if len(sys.argv) == 2:
+    wd = sys.argv[1]
+else:
+    wd = input("to continue, press enter (or specify directory):")
+if os.path.exists(wd) or wd == "":
+    if not wd == "":
+        os.chdir(wd)
+    else:
+        wd = "."
+    for entry in os.listdir(wd):
+        if not os.path.isfile(os.path.join(".", entry)) and entry != ".git":
+            fromDirectory = "./" + entry
+            toDirectory = "../calibre_usb"
+            # copy_tree(fromDirectory, toDirectory)
+            print('from:' + fromDirectory + '; to: ' + toDirectory)
+            to_be_del.append(entry)
+    print('-----------------------------------')
+    for i in range(len(to_be_del)):
+        print(to_be_del[i])
+    print("sure to delete above items?")
+    confirm = input()
+    if confirm == "":
+        print('to delete')
+        for i in range(len(to_be_del)):
+            # send2trash("./"+to_be_del[i])   #need"./"?
+            print("./"+to_be_del[i])
+    else:
+        print("items kept.")
+else:
+    print("invalid path")
 
-for entry in os.listdir():
-    if not os.path.isfile(os.path.join(".", entry)):
-        fromDirectory = "./" + entry
-        toDirectory = "../calibre_usb"
 
-        copy_tree(fromDirectory, toDirectory)
+#allow multiple attempt if typo
+
+
